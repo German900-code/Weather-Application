d7dd606f4594819adab02a8dcb3a9fcbca3ab87a
German Voloshyn <germanredmi13@gmail.com>
2026-04-30 23:39:45 +0300

    Delete WeatherApp.jsx file, no longer needed

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 import "./index.css";
-import WeatherApp from "./components/WeatherApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <WeatherApp /> */}
     <App />
   </StrictMode>,
 );
